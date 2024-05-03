ad3bee19308f127db313ce03a3a391c806dd4696
michelealiffi <michelealiffi890@gmail.com>
2024-05-03 12:34:13 +0200

    Snack 3 update

---
+++ JSnack3/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+let userNumber1 = Number(prompt('Inserire un numero'));
+let userNumber2 = Number(prompt('Inserire un numero'));
+let userNumber3 = Number(prompt('Inserire un numero'));
+let userNumber4 = Number(prompt('Inserire un numero'));
+let userNumber5 = Number(prompt('Inserire un numero'));
+let userNumber6 = Number(prompt('Inserire un numero'));
+let userNumber7 = Number(prompt('Inserire un numero'));
+let userNumber8 = Number(prompt('Inserire un numero'));
+let userNumber9 = Number(prompt('Inserire un numero'));
+let userNumber10 = Number(prompt('Inserire un numero'));
+
+let userNumberTotal = userNumber1 + userNumber2 + userNumber3 + userNumber4 + userNumber5 + userNumber6 + userNumber7 + userNumber8 + userNumber9 + userNumber10;
+
+console.log(userNumberTotal);
