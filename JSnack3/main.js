b1f6ad2f26d54fada9c69f76cf439b59b27a62d2
michelealiffi <michelealiffi890@gmail.com>
2024-05-03 13:25:18 +0200

    Up

--- JSnack3/main.js
+++ JSnack3/main.js
@@ -1,16 +1,9 @@
 'use strict'
 
-let userNumber1 = Number(prompt('Inserire un numero'));
-let userNumber2 = Number(prompt('Inserire un numero'));
-let userNumber3 = Number(prompt('Inserire un numero'));
-let userNumber4 = Number(prompt('Inserire un numero'));
-let userNumber5 = Number(prompt('Inserire un numero'));
-let userNumber6 = Number(prompt('Inserire un numero'));
-let userNumber7 = Number(prompt('Inserire un numero'));
-let userNumber8 = Number(prompt('Inserire un numero'));
-let userNumber9 = Number(prompt('Inserire un numero'));
-let userNumber10 = Number(prompt('Inserire un numero'));
+let sum = 0;
 
-let userNumberTotal = userNumber1 + userNumber2 + userNumber3 + userNumber4 + userNumber5 + userNumber6 + userNumber7 + userNumber8 + userNumber9 + userNumber10;
+for(let i = 0; i < 10; i++) {
+    sum += Number(prompt(`Inserire un numero ${i}`));
+}
 
-console.log(userNumberTotal);
+console.log(sum);
